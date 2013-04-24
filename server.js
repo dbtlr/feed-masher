7d92c18ed4936ef38a68e9915cf4d1e85c7b5078
Drew Butler <drew@nodrew.com>
2013-04-24 11:55:05 -0400

    Added a basic server job runner.

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var environment = require('./config/environment');
+var environment = require('./config/environment')
+  , jobsPath = __dirname + '/app/jobs'
+  , argv = require('optimist')
+      .usage('Start local server environment')
+      .argv
+;
 
 console.log('Loading Feed Process Server');
 
 environment.process();
+
+argv._.forEach(function(option) {
+  switch (option) {
+    case 'update':
+      require(jobsPath + '/updateFeed').run();
+      break;
+  }
+});
