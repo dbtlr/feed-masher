e7d685fc24a87dc17be36dc4a00829788362f12a
Drew Butler <drew@nodrew.com>
2013-04-24 13:34:08 -0400

    Format return data.

--- app/models/service/twitter.js
+++ app/models/service/twitter.js
@@ -4,10 +4,17 @@ var Twit = require('twit')
 exports.update = function() {
   config.twitter.forEach(function(config) {
     var ts = new Twit(config.credentials);
-  
+
     ts.get('statuses/user_timeline', { screen_name: config.screen_name, count: 10 }, function(err, reply) {
       reply.forEach(function(item) {
-        console.log(item.text);
+        var data = {
+          original_id: item.id,
+          text: item.text,
+          source: item.source,
+          url: 'http://twitter.com/' + item.user.screen_name + '/status/' + item.id,
+        };
+        
+        // Todo: save data to Mongo, when a status is new.
       });
     });
   });
